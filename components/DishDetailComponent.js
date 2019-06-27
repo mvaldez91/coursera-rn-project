2f36b2b775247126a0a6c1dfbd07f574568823f4
Marco <mtulio1991@live.com>
2019-06-26 20:47:39 -0600

    First commit

---
+++ components/DishDetailComponent.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Card} from 'react-native-elements';
+
+function RenderDish(props){
+    const dish = props.dish;
+    let result = {};
+
+    if (dish!= null){
+        result =
+            <Card featuredTitle={dish.name}
+                  image={require('../images/uthappizza.png')}
+            >
+                <Text style={{margin: 10}}>
+                {dish.description}
+                </Text>
+            </Card>;
+    }
+    else{
+        result = <View></View>;
+    }
+
+    return result;
+}
+
+function DishDetail(props){
+    return(<RenderDish dish={props.dish}/>);
+}
+
+export default DishDetail;
---
+++ readme.md
@@ -0,0 +1,7 @@
+#Instructions:
+
+#npm install
+
+#set REACT_NATIVE_PACKAGER_HOSTNAME=your.current.ip
+
+#npm start
